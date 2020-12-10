5e1a6b8ff44fe11e0a29dc521cfce4b1135f2b0d
Choubey <52009026@acsind.com>
2020-12-10 17:51:56 +0530

    Initial commit

---
+++ app/src/main/java/com/amod/listview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.amod.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Array of strings...
+    String[] mobileArray = {"Amod", "Aman", "Antu", "Dhirendra",
+            "Situ", "Pilatu", "Gabar", "Mantosh"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+    }
+}
